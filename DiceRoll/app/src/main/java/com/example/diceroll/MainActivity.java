00b96501d1d12dde5d23be5adfe0e92412351406
TharunSaravanan <bstharun@gmail.com>
2020-12-26 10:46:56 -0600

    dice roller app

--- DiceRoll/app/src/main/java/com/example/diceroll/MainActivity.java
+++ DiceRoll/app/src/main/java/com/example/diceroll/MainActivity.java
@@ -16,23 +16,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+
         TextView btnExample = findViewById(R.id.txtFlip);
         btnExample.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Random rand = new Random();
-                int value = rand.nextInt(2);
-                ImageView coin = findViewById(R.id.coinImage);
-
-
-                if(value == 0){
-                    coin.setImageResource(R.drawable.heads);
-                }
-                else {
-                    coin.setImageResource(R.drawable.tails);
-                }
+                rollDice();
             }
         });
+    }
 
+    private void rollDice() {
+
+        /*findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        findViewById(R.id.loadingPanel).setVisibility(View.GONE);*/
+
+
+        Random rand = new Random();
+        int value = rand.nextInt(6) + 1;
+        ImageView coin = findViewById(R.id.coinImage);
+
+        switch(value) {
+            case 1:
+                coin.setImageResource(R.drawable.dice_1);
+                break;
+            case 2:
+                coin.setImageResource(R.drawable.dice_2);
+                break;
+            case 3:
+                coin.setImageResource(R.drawable.dice_3);
+                break;
+            case 4:
+                coin.setImageResource(R.drawable.dice_4);
+                break;
+            case 5:
+                coin.setImageResource(R.drawable.dice_5);
+                break;
+            case 6:
+                coin.setImageResource(R.drawable.dice_6);
+                break;
+        }
     }
 }
