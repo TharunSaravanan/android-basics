ce91583cf778d7c31e4d7a890bc35bf43e5f7ac3
TharunSaravanan <bstharun@gmail.com>
2021-02-14 20:38:06 -0600

    Update MainActivity.java

--- ButtonCounterApp/app/src/main/java/com/example/buttoncounterapp/MainActivity.java
+++ ButtonCounterApp/app/src/main/java/com/example/buttoncounterapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.buttoncounterapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText userInput;
+    private Button button;
+    private TextView textView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
     }
 }
